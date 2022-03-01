d8155d280f856a1040138ee909df11a46d2c292c
Forszt Mihály <mihalyforszt@outlook.hu>
2022-03-01 12:26:19 +0100

    új navbar, ikon, login page, mobil nézet javítások

---
+++ StudentRegister/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using StudentRegister.Data;
+
+[assembly: HostingStartup(typeof(StudentRegister.Areas.Identity.IdentityHostingStartup))]
+namespace StudentRegister.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<StudentRegisterContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("StudentRegisterContextConnection")));
+
+                //services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                //    .AddEntityFrameworkStores<StudentRegisterContext>();
+            });
+        }
+    }
+}
--- StudentRegister/obj/Debug/net5.0/Razor/Areas/Identity/Pages/_ViewStart.cshtml.g.cs
+++ StudentRegister/obj/Debug/net5.0/Razor/Areas/Identity/Pages/_ViewStart.cshtml.g.cs
@@ -12,7 +12,29 @@ namespace AspNetCore
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Mvc.Rendering;
     using Microsoft.AspNetCore.Mvc.ViewFeatures;
+#nullable restore
+#line 1 "C:\Users\Forszt\Downloads\StudentRegister_GroupProject-master\StudentRegister\Areas\Identity\Pages\_ViewImports.cshtml"
+using Microsoft.AspNetCore.Identity;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 2 "C:\Users\Forszt\Downloads\StudentRegister_GroupProject-master\StudentRegister\Areas\Identity\Pages\_ViewImports.cshtml"
+using StudentRegister.Areas.Identity;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 3 "C:\Users\Forszt\Downloads\StudentRegister_GroupProject-master\StudentRegister\Areas\Identity\Pages\_ViewImports.cshtml"
+using StudentRegister.Areas.Identity.Pages;
+
+#line default
+#line hidden
+#nullable disable
     [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"330a635f71f47285eacb2484685031fe87558a73", @"/Areas/Identity/Pages/_ViewStart.cshtml")]
+    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"3a62bd9fc5525e1f177a76be9b483d6576befd97", @"/Areas/Identity/Pages/_ViewImports.cshtml")]
     public class Areas_Identity_Pages__ViewStart : global::Microsoft.AspNetCore.Mvc.Razor.RazorPage<dynamic>
     {
         #pragma warning disable 1998
